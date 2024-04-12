f70377556d4bea70ed643b14737b9a6bcf7f3840
kjeff24 <jeffreyarthur123@gmail.com>
2024-04-12 17:34:57 +0000

    Added a bootstrap refresh scope

---
+++ product-service/src/main/java/com/bexos/productservice/controllers/CategoryController.java
@@ -0,0 +1,24 @@
+package com.bexos.productservice.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@Getter
+@Setter
+@RefreshScope // This will refresh all the @value data in this bean after /actuator/refresh is called
+public class CategoryController {
+    @Value("${app.prop1}")
+    private String prop1;
+
+    @GetMapping("/prop")
+    public String getProp1(){
+        return prop1;
+    }
+}
